db251a44083579331efadeb8d47bf114381c3d29
murilogalhardo <murilo_galhardo@hotmail.com>
2024-10-15 10:30:15 -0300

    Implementado o código funcional e o estruturado no math.js

--- src/math.js
+++ src/math.js
@@ -9,6 +9,10 @@
  */
 function somaDobroNumerosParesFuncional(numeros){
     // TODO: Insira seu código funcional aqui
+    return numeros
+        .filter(num => num % 2 === 0) // seleciona números pares
+        .map(num => num * 2)          // dobra o valor de cada um
+        .reduce((acc, num) => acc + num, 0); // soma todos os valores dobrados
 }
 
 /**
@@ -17,7 +21,13 @@ function somaDobroNumerosParesFuncional(numeros){
  * @param {Array} numeros vetor com os números desejados
  */
 function somaDobroNumerosParesProcedural(numeros){
-    // TODO: Insira seu código estruturado aqui   
+    // TODO: Insira seu código estruturado aqui
+    let soma = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 === 0) {  // verifica se é par
+            soma += numeros[i] * 2;  // dobra o valor e acumula
+        }
+    }
+    return soma;
 }
-
 export { somaDobroNumerosParesFuncional, somaDobroNumerosParesProcedural };
